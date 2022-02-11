54856092767f9434305d73fdd42a6e1dc83bf6e1
Syntax <muhammadmasoom1001@gmail.com>
2022-02-11 09:07:37 +0000

    Update index.js

--- websocket/index.js
+++ websocket/index.js
@@ -1,58 +1,28 @@
-"use strict";
-
-const serverPort = 8080,
-    http = require("http"),
-    express = require("express"),
-    app = express(),
-    server = http.createServer(app),
-    WebSocket = require("ws"),
-    websocketServer = new WebSocket.Server({ server });
-
-//when a websocket connection is established
-websocketServer.on('connection', (webSocketClient) => {
-    //send feedback to the incoming connection
-    webSocketClient.send('{ "connection" : "ok"}');
-    
-    //when a message is received
-    webSocketClient.on('message', (message) => {
-
-        //for each websocket client
-        websocketServer
-        .client
-        .forEach( client => {
-            //send the client the current message
-            client.send(`${message}`);
+// Importing the required modules
+const WebSocketServer = require('ws');
+ 
+// Creating a new websocket server
+const wss = new WebSocketServer.Server({ port: 8080 })
+ 
+// Creating connection using websocket
+wss.on("connection", ws => {
+    console.log("new client connected");
+    // sending message
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+        wss.clients.forEach(function each(client) {
+            client.send(`${data}`);
         });
     });
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
 });
+console.log("The WebSocket server is running on port 8080");
 
-//start the web server
-server.listen(serverPort, () => {
-    console.log(`Websocket server started on port ` + serverPort);
-});
-
-
-const port = process.env.PORT || 3000
-const bodyParser = require("body-parser");
-const fs = require("fs");
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static("public"))
-app.use(express.static("node_modules"))
-
-app.get('/', (a, b) => {
-  fs.createReadStream("public/index.html").pipe(b)
-});
-
-app.listen(port, () => {
-  console.log(`Clientserver has opened on http://localhost:${port}`);
-  console.log("Everything up and running!")
-  console.warn("Do not close this, doing so will result in disconnection between the script and external UI.")
-  console.log(`
-                                                                           
- 
-                                                                                                  
-                                                                                                                                                                                                                         
-  `);
-});
+// client.send(`${message}`);
